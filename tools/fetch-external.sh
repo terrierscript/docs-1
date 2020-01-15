d610c693f9a2d07e180a70054229e820d11af542
Gareth Davies <hello@garethtdavies.com>
2020-01-15 11:47:01 -0800

    partials: Import known tested modems from meta-balena
    Co-authored-by: Matthew McGinn <matthew@balena.io>
    Change-type: patch
    Signed-off-by: Gareth Davies <gareth@balena.io>

--- tools/fetch-external.sh
+++ tools/fetch-external.sh
@@ -2,28 +2,54 @@
 # This script pulls in external documentation that should be edited in the corresponding upstream repo
 
 # get latest CLI docs
-cd pages/reference/ && { curl -O -L https://github.com/balena-io/balena-cli/raw/master/doc/cli.markdown ; mv cli.markdown cli.md ; cd -; }
+cd pages/reference/ && {
+  curl -O -L https://github.com/balena-io/balena-cli/raw/master/doc/cli.markdown
+  mv cli.markdown cli.md
+  cd -
+}
 
 # get latest node SDK docs
 cd pages/reference/sdk/ && {
-  curl -O -L https://github.com/balena-io/balena-sdk/raw/master/DOCUMENTATION.md;
-  echo "# Balena Node.js SDK" > node-sdk.md
-  tail -n +2 DOCUMENTATION.md >> node-sdk.md;
-  cd -;
+  curl -O -L https://github.com/balena-io/balena-sdk/raw/master/DOCUMENTATION.md
+  echo "# Balena Node.js SDK" >node-sdk.md
+  tail -n +2 DOCUMENTATION.md >>node-sdk.md
+  cd -
 }
 
 # get latest python SDK docs
 cd pages/reference/sdk/ && {
-  curl -O -L https://github.com/balena-io/balena-sdk-python/raw/master/DOCUMENTATION.md;
-  mv DOCUMENTATION.md python-sdk.md;
-  cd -;
+  curl -O -L https://github.com/balena-io/balena-sdk-python/raw/master/DOCUMENTATION.md
+  mv DOCUMENTATION.md python-sdk.md
+  cd -
 }
 
 # get latest supervisor API docs
-cd pages/reference/supervisor/ && { curl -O -L https://github.com/balena-io/balena-supervisor/raw/master/docs/API.md; mv API.md supervisor-api.md ; cd -; }
+cd pages/reference/supervisor/ && {
+  curl -O -L https://github.com/balena-io/balena-supervisor/raw/master/docs/API.md
+  mv API.md supervisor-api.md
+  cd -
+}
 
 # get latest diagnostics docs
-cd pages/reference/ && { curl -O -L https://github.com/balena-io/device-diagnostics/raw/master/diagnostics.md; cd -; }
+cd pages/reference/ && {
+  curl -O -L https://github.com/balena-io/device-diagnostics/raw/master/diagnostics.md
+  cd -
+}
 
 # get latest supervisor update-lock docs
-cd pages/learn/deploy/release-strategy/ && { curl -O -L https://github.com/balena-io/balena-supervisor/raw/master/docs/update-locking.md; cd -; }
+cd pages/learn/deploy/release-strategy/ && {
+  curl -O -L https://github.com/balena-io/balena-supervisor/raw/master/docs/update-locking.md
+  cd -
+}
+
+# get meta-balena README and extract partials
+cd shared/meta-balena/ && {
+  curl -O -L https://raw.githubusercontent.com/balena-os/meta-balena/development/README.md
+  mv README.md meta-balena.md
+  # Extract modem text
+  ../../tools/extract-markdown.sh "Modems" < meta-balena.md > supported-modems.md
+  # Extract Wifi adapters   
+  ../../tools/extract-markdown.sh "WiFi Adapters" < meta-balena.md > supported-wifi-adapters.md
+  # Extract config.json
+  ../../tools/extract-markdown.sh "config.json" < meta-balena.md > config-json.md
+}
